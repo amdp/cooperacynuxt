3ae764088c9268f617f7fe9567bbd2aa59e16eec
Alessandro Merletti De Palo <alessandromerlettidepalo@gmail.com>
2019-05-18 16:11:13 +0200

    fixed fetch, comment, project

--- store/index.js
+++ store/index.js
@@ -16,18 +16,17 @@ export const state = () => ({
 export const getters = { }
 export const mutations = { 
   setProptype:  (state,payload) => {state[payload.proptype] = payload.body},
-  setUservote:  (state,payload) => {state[payload.proptype + 'uservote'] = payload.body},
+  setUservote:  (state,payload) => {state[payload.proptype + 'uservote'] = payload.body },
   setTag:       (state,payload) => {state.tag = payload},
   setCategory:  (state,payload) => {state.category = payload},
   editSwitch:   (state,payload) => {state.edit = payload},
-  commentUpdate:(state,payload) => {state.comment.push({
-    id:payload.id, parent:payload.parent, project:payload.project, content:payload.content, E:0,T:0,C:0,I:0,F:0,U:0,D:0})},
+  commentUpdate:(state,payload) => {state.comment.push(payload)},
   voteUpdate:   (state,payload) => {//updates the voted proptype (project or comment) state and uservote table
     var votedProptype = state[payload.proptype].find(proptype => proptype.id == payload.id)
     votedProptype[payload.cc] += payload.add //at this point we have updated the proptype, we proceed with the uservote table:
     payload.add == 1 ? state[payload.proptype+'uservote'].push({user: payload.user, [payload.proptype]: payload.id, condition: payload.cc})
-    : state[payload.proptype+'uservote'].splice(
-      state[payload.proptype+'uservote'].findIndex(uservote => uservote[payload.proptype] == payload.id && uservote.condition == payload.cc),1)
+    : state[payload.proptype+'uservote'].splice(state[payload.proptype+'uservote'].findIndex(
+      uservote =>uservote[payload.proptype]==payload.id && uservote.condition == payload.cc),1)
   },
 }
 
@@ -38,29 +37,30 @@ export const actions = {
   getProptypeAction: async (context,payload) => {
     let {data} = await axios({url:'http://127.0.0.1:3000/db/proptype', params: payload})
     let go = {body: data, proptype: payload.proptype}
-    let gouser = {id: payload.userid, proptype: payload.proptype}
-    if (payload.where){gouser.where = payload.where}
-    context.dispatch('getUservoteAction', gouser )
+    let gouser = {userid: payload.userid, proptype: payload.proptype}
+    if (payload.limit){gouser.projectid = payload.projectid; gouser.limit = payload.limit}
+    await context.dispatch('getUservoteAction', gouser )
     context.commit('setProptype', go)
   },
 
-  projectFormAction: async (context,payload) => {let {data} = await axios.post('http://127.0.0.1:3000/db/project', payload)
-    if (data.id){return data.id}else{return data} },
-
-  commentFormAction: async (context,payload) => {let {data} = await axios.post('http://127.0.0.1:3000/db/comment', payload)
-    payload.id=data.id; context.commit('commentUpdate', payload) },
-
-////////////////////////////// VOTE //////////////////////////////
-
-  // this action GETS THE VOTE
+  // this action GETS THE USER VOTE
   getUservoteAction: async (context, payload) => {
     let {data} = await axios({url:'http://127.0.0.1:3000/db/uservote', params: payload})
     let go = {body: data, proptype: payload.proptype}
     if (payload.where){go.where = payload.where}
     context.commit('setUservote', go)
+    return
   },
+
   // this action EDITS THE VOTE
   addVoteAction : async (context,payload) => {let {data} = await axios.post('http://127.0.0.1:3000/db/vote', payload); return data },
+
+  projectFormAction: async (context,payload) => {let {data} = await axios.post('http://127.0.0.1:3000/db/project', payload)
+    if (data.id){return data.id}else{return data} },
+
+  commentFormAction: async (context,payload) => {let {data} = await axios.post('http://127.0.0.1:3000/db/comment', payload)
+  console.log('data to be commited: '+JSON.stringify(data));
+    context.commit('commentUpdate', data) },
 
 ////////////////////////////// USER //////////////////////////////
 
