b0e7eaddd43148dab7c9027ad6c71de1318f5d3f
Alessandro Merletti De Palo <alessandromerlettidepalo@gmail.com>
2019-05-30 21:01:25 +0200

    better project edit

--- pages/project/_id.vue
+++ pages/project/_id.vue
@@ -4,7 +4,7 @@
     <div class="row mt-5" v-for="project in this.$store.state.project" :key="project.id">
       <div class="col-12">
         <div class="row">
-          <div class="col-1"><nuxt-link to="/project/form" >EDIT</nuxt-link></div>
+          <div class="col-1"><button @click="edit">EDIT</button></div>
           <div class="col-2">
             <img :src="require('@/assets/image/project/' + project.id + '.png')" width="100px" />
           </div>
@@ -31,6 +31,7 @@ import Comment from '@/components/Comment'
 
 export default {
   middleware: ['auth'], components: { Votebar: Votebar, Comment: Comment, },
+  methods: {edit(){this.$store.dispatch('editSwitchAction', this.$route.params.id); return this.$router.push({path: '/project/form'})}},
   async fetch ({store, params}){ 
     await store.dispatch('getProjectAction', { projectid: params.id, limit:' LIMIT 1', userid: store.state.auth.user.id,}) 
     await store.dispatch('getCommentAction', { projectid: params.id, limit:' ',        userid: store.state.auth.user.id,})},
