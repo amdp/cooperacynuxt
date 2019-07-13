aeeccb1a00ea224e687dc3c693a2b44a284b442e
Alessandro Merletti De Palo <alessandromerlettidepalo@gmail.com>
2019-07-13 14:05:13 +0200

    paypal #1 and security

--- components/Newuser.vue
+++ components/Newuser.vue
@@ -17,16 +17,32 @@
             <b-form-group label-for="imageInput" label="Image:" description="Please choose an image for your account"> 
               <b-form-file id="imageInput" v-model="formImageFile" ref="formImageFile" size="sm"></b-form-file>
             </b-form-group>
-            <b-button type="submit" class="btn bhcare btn-block mt-3 white border-0">JOIN</b-button>
+            <b-button type="submit" class="btn bhcare btn-block mt-3 white border-0">JOIN WITH BANK TRANSFER</b-button><br>
+            <p class="subheading d-flex justify-content-center">OR WITH</p>
           </b-form>
         </div>
       </div>
+      <div class="d-flex justify-content-center" id="paypal-button-container"></div>
+      <script>paypal.Buttons({
+        //onError: function (err) {Show an error page here, when an error occurs}
+        //onCancel: function (data) {When a buyer cancels a payment, they typically return to the parent page. You can instead indicate a different page here.}
+        // REMOVE DEBUG!!
+        createSubscription: function(data, actions) { return actions.subscription.create({ 'plan_id': 'P-9C681042E7918904VLURYYGQ'});},
+        onApprove: function(data, actions) {alert('You have successfully become a member with subscription ID ' + data.subscriptionID);
+        this.formPaypalagreementid=data.subscriptionID; this.newuser();}}).render('#paypal-button-container');
+      </script>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  head: {
+    title: 'Join or Login',
+    script: [{src:'https://www.paypal.com/sdk/js?client-id='+process.env.PAYPALID+'&vault=true&currency=EUR&debug=true'}], //live
+    //script: [{src:'https://www.paypal.com/sdk/js?client-id='+process.env.PAYPALID+'&vault=true&currency=EUR&debug=true'}], //sandbox
+    meta: [ { name: 'viewport', content: 'width=device-width, initial-scale=1' }, { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },]
+  },
   data() {
     return {
       formName: '',
@@ -34,6 +50,7 @@ export default {
       formEmail: '',
       formPassword: '',
       formImageFile: '',
+      formPaypalagreementid: 'bank',
     }
   },
   methods: {
@@ -43,6 +60,7 @@ export default {
         surname: this.formSurname,
         email: this.formEmail,
         password: this.formPassword,
+        paypalagreementid: this.formPaypalagreementid
       })
       if (newuser == 'exists'){return this.$toast.show('Email already in use!', {duration: 1000, className: 'toasts'})}
       if (this.formImageFile) {
@@ -67,6 +85,52 @@ export default {
       this.$toast.success('New user added.', {duration: 1000, className: 'toasts'})
       setTimeout(function(){location.href = location.href}, 1200)
     }
-  }
+  },
+
 }
 </script>
+    <!-- <div v-if="this.$store.state.payment=='cancelled'"><p class="base understanding center space up">Subscription cancelled.<br><br>
+    You have {{ this.$store.state.payment.days }} days left to join us!</p></div>
+    <div v-else>
+    <p class="base equivalence center space up">Step 2: the voluntary pooling<br><br></p>
+    <p class="base">The Cooperacy membership is based on a voluntary pooling of just one euro per month: the money is pooled to finance future ideas and projects, included yours.<br><br>
+
+    <nuxt-link href="paypalpay" class="btn btn-primary btn-block">VOLUNTARY POOLING THROUGH PAYPAL</nuxt-link>
+    <p class="mini">If you encounter any problem please screenshot any error and.. write us! (see below)</p><br><br>
+
+    Living in a low income country? You can pool with a lower amount if you help us adapt the amount to the different purchasing power of your nation. Write us.<br><br>
+
+    If you don't like paypal, you can pool one year membership (12 euros) via bank transfer: our IBAN is IT61D0623001623000043225634 - Filiale AG 23 Milano Cariparma di via della Moscova.<br><br>
+
+    <p class="base">Pool instead via bank transfer sending us a code/receipt or contact us for suggestions, questions, requests:</p>
+    <div role="form" class="wpcf7" id="wpcf7-f1303-p20-o1" lang="en-US" dir="ltr">
+
+    <p class="mini">Your Name (required)<br />
+    {{ Form::text('name', null, [
+    'size' => 40,
+    'class' => 'wpcf7-form-control wpcf7-text wpcf7-validates-as-required',
+    'aria-required' => 'true',
+    'aria-invalid' => 'false',]) }}</p>
+
+    <p class="mini">Your Email (required)<br />
+    <span class="wpcf7-form-control-wrap your-email">{{ Form::email('email', null, [
+    'size' => 40,
+    'class' => 'wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email',
+    'aria-required' => 'true',
+    'aria-invalid' => 'false',]) }}</span> </label></p>
+
+    <p class="mini">Subject<br />
+    <span class="wpcf7-form-control-wrap your-subject">{{ Form::text('subject', null, [
+    'size' => 40,
+    'class' => 'wpcf7-form-control wpcf7-text',
+    'aria-invalid' => 'false',]) }}</span></p>
+
+    <p class="mini">Your Message<br />
+    <span class="wpcf7-form-control-wrap your-message">{{ Form::textarea('message', null, [
+    'cols' => 40,
+    'rows'=>10,
+    'class' => 'wpcf7-form-control wpcf7-textarea',
+    'aria-invalid' => 'false',]) }}</span></p>
+
+    <p class="mini"><input type="submit" value="Send" class="wpcf7-form-control wpcf7-submit" /></p>
+    <div class="wpcf7-response-output wpcf7-display-none"></div>-->
