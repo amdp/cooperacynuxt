58a5f453005cb077d94af0fa95c01951f46f6419
Alessandro Merletti De Palo <alessandromerlettidepalo@gmail.com>
2019-07-21 20:53:34 +0200

    static pages and google id

---
+++ pages/participate.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+  <br /><img class="page-header" src="~/assets/image/page/banners/participate.png" />
+
+  <p class="base">Cooperacy activities are growing! You can support our efforts by joining one of them:<br></p>
+  <p class="subheading equivalence center space margin">THE PLATFORM</p>
+  <p class="base">Our main effort is to finish the development of our ideas <a class="ae" href="/platform">platform</a>. We have trialled some activities which have created surplus and this has been returned to the Cooperacy pool. We invite members to propose further ideas that will stimulate sustainable surplus growth. Although we have already made some testing we still have to reach 3000 members to make the platform appealing, dynamic and sustainable. This is our first milestone, to get more participants: we are currently aroud 50, coming from Italy, Germany, United States, Brazil, Iran!</p>
+
+  <div class="progress">
+  <div class="progress-bar" role="progressbar" aria-valuenow="1%"
+  aria-valuemin="0" aria-valuemax="100" style="width:1%; color:#000000; background-color: #FF9000">&nbsp;1%&nbsp;Complete</div></div>
+
+  <p class="subheading equivalence center space margin">COOPERACY MESSENGERS</p>
+  <p class="base">Our communication group needs new members to help with the texts, the graphics, the translations and the front end coding. If you have any of these skills and you are willing to participate please <a class="ae" href="/register">join</a> and <a class="at" href="/contact">contact us.</a></p>
+
+  <p class="subheading equivalence center space margin">COOPERACY CODERS</p>
+  <p class="base">If we had enough coders we would need a lot less than 3000 members to support and maintain the platform. If you would like to participate in an open project in which your experience will be of great value for the group while the final output will be a human centered designed tool to foster human cooperation, we are really happy to introduce you to our <a class="at" href="/contact">coding team</a>.</p>
+
+  <p class="subheading equivalence center space margin">COOPERACY EDUCATION</p>
+  <p class="base"><a class="at" href="/contact">Help us</a> with the online live webinars and in preparing the materials for the courses!</p>
+
+  <p class="subheading equivalence center space margin">COOPERACY ACROYOGA</p>
+
+  <p class="base">Acroyoga merges acrobatic movements with simple yoga positions. It is based on the <a class="ae" href="/cooperation">cooperation</a> and <a class="at" href="/cooperation#trust">trust</a> between two people and one supervisor. The cooperation in this case is based on how the bodies balances depend on the reciprocal <a class="au" href="/cooperation#understanding">understanding</a> of the participants. It is an activity that highlights the values of Cooperacy and has been developed following the Cooperacy governance system.</p>
+  <div class="center"><video width="480px" height="360px" controls>
+  <source src="~/assets/image/page/participate/acrocoo.mp4" type="video/mp4" alt="null">
+  </video></div><br>
+  <p class="base">This project was led by Igor Carravetta and Carola Ravaschino in 2016. During one of the first Cooperacy meetings, we were discussing how to test the <a class="af" href="/governance">voting system</a> and do something together. The group voted and we funded and helped the weekly course from October to March. It has been an amazing experience. Igor and Carola are happy to start over, if you want just drop a <a class="at" href="/contact">line</a> asking for them.</p>
+
+  <p class="subheading equivalence center space margin">EXPERIMENTAL PROJECTS</p>
+  <p class="base">This is a non-exaustive list of the projects we are currently testing.</p>
+  <div class="center"><img width="480px" height="360px" src="~/assets/image/page/participate/coofood.png"><br><br></div>
+  <p class="mini"><span class="freedom">1.</span> In Milan we bought fruit and vegetables directly from direct sources. We sold them amongst our members and made 30 Euros. This covered all expenses for the experiment including transport and people’s time.<br>
+  <span class="freedom">2.</span> We have researched and found that we are able to organise and develop a business out of supplying delicious, organic, multi-cereal bread from Busto Arsizio. If you would like to drive this exciting project let us know!<br>
+  <span class="freedom">3.</span> Experience has shown us that we are able to earn 100 Euros for each catering event we organise.<br><br>
+  As you can see, we have had fun experimenting. Would you like to test your own? <a class="at" href="/contact">Write us</a> so that we can talk together and organise how to do it!<br><br>
+  </p>
+
+  <p class="subheading equivalence center space margin">THE ORGANIZATIONS SIDE</p>
+  <p class="base">Cooperation science can <a class="au" href="/services">improve</a> the productivity of your company and the satisfaction of your employees. It could, on a wider scale, even help the world to gain a more respectful awareness accompained with better performances and more sustainability. Are you part of the corporate world? Would you like to apply the Cooperation Science in your company processes? Feel free to <a class="at" href="/contact">contact us</a> and ask for more information!</p>
+
+  <p class="subheading equivalence center space margin">COOPERATION SCIENCE</p>
+  <p class="base">This project is the first and most important one as Cooperacy was born from our scientific research about Cooperation.<br><br></p>
+  <p class="base">Alessandro Merletti De Palo and Ilario Tito were friends since 35 years. In 2014 they met in Rome, at Bar della Pace, to talk about how much the social system needed a change. A serious talking amongst friends, the typical chat that ends there once you say goodbye. Instead, when Alessandro went back home, he kept on thinking about it. How do you call that special, enjoyable energy that you feel with your friends even when you do something useful? There was no specific emotion, but Alessandro thought about the word "<a class="ae" href="/cooperation">cooperation</a>". What is cooperation? Can we bring that emotion of doing things together with enjoyment at a worldwide level? He wanted to know more, but there was no "Faculty of Cooperation" or "Cooperation University". So he started with the simplest thing he could do. He looked for the word cooperation in the dictionary. There were many definitions, but none was clear. He started reading scientific articles about it, intertwining different disciplines. Game Theory, Philosophy, Sociology, Psychology, Economy, Evolutionary Biology, Linguistics, Anthropology, Neuroscience.. there were many different approaches to the study of cooperation. Merging them together in a model, Alessandro called Ilario back and asked him to cooperate together and find eventual demonstration of the theory, in order to to introduce it at the MIT Collective Intelligence conference. Together with Maria Gisa Masia, Alessandro Mancinella, Mariangela Nitti, and Kanwar Ujjawal Singh they wrote a paper that was accepted at the conference.  Since then, they participated to many other conferences around the world: this story tells the birth of the scientific model behind the idea of Cooperacy. That meeting grew into more than a great meeting between friends, it was the seed from which the rest of Cooperacy has grown from. Out of our own friendship we believe we can grow the enjoyment, satisfaction and contentment of Cooperacy world wide.
+  Today we’re doing great with Cooperation Science! Our model was at two conferences by MIT in the US, and in 2017 we presented in Macao our international Cooperation Context Index by countries. Have a look at the <a class="au" href="/coscience">scientific</a> part of our website and, if you are an academic, please <a class="at" href="/register">join</a> Cooperacy and our team of researchers!</p><br><br>
+  </div>
+</template>
+  
