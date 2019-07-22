103552a2d9fe27395b4c14a6a36bae3f1cad972f
Alessandro Merletti De Palo <alessandromerlettidepalo@gmail.com>
2019-07-22 13:31:29 +0200

    db fix

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const nodemailer = require("nodemailer");
 const Jimp = require('jimp');
 const cc=['D','U','F','I','C','T','E']
 const mysql = require("mysql2"); const mydb = mysql.createConnection({ connectionLimit: 200, host:process.env.HOST, 
-  user: process.env.MYSQLUSER, password: process.env.DBPASSWORD, database: process.env.DBDB˝, multipleStatements: true})
+  user: process.env.MYSQLUSER, password: process.env.DBPASSWORD, database: process.env.DBDB, multipleStatements: true})
 
 
 var axios=require("axios")
