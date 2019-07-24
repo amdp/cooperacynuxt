a778c1c4642b157f3b97ec78aa905eba01fbcb8e
Alessandro Merletti De Palo <alessandromerlettidepalo@gmail.com>
2019-07-24 22:28:34 +0200

    various fixes

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const bcrypt = require("bcrypt")
 const nodemailer = require("nodemailer");
 const Jimp = require('jimp');
 const cc=['D','U','F','I','C','T','E']
-const mysql = require("mysql2"); const mydb = mysql.createConnection({ connectionLimit: 200, host:process.env.HOST, 
+const mysql = require("mysql2"); const mydb = mysql.createConnection({ connectionLimit: 200, host: 'localhost', 
   user: process.env.MYSQLUSER, password: process.env.DBPASSWORD, database: process.env.DBDB, multipleStatements: true})
 var axios=require("axios")
 
