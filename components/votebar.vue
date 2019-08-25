8f570fd96b6af00ff2e99e60986c383c5133dddd
Alessandro Merletti De Palo <alessandromerlettidepalo@gmail.com>
2019-08-25 21:44:07 +0200

    new menu layout, new font css names

--- components/votebar.vue
+++ components/votebar.vue
@@ -1,13 +1,6 @@
-
 <template>
-  <b-row
-    class="p-0 w-100 m-0"
-    v-if="this.proptype=='user'"
-  >
-    <b-col
-      cols="12"
-      class="p-0 m-0 w-100 d-flex"
-    >
+  <b-row class="p-0 w-100 m-0" v-if="this.proptype == 'user'">
+    <b-col cols="12" class="p-0 m-0 w-100 d-flex">
       <div
         v-for="vote in this.vote"
         :key="vote.userkey"
@@ -16,10 +9,7 @@
       ></div>
     </b-col>
   </b-row>
-  <b-row
-    class="mt-2"
-    v-else-if="this.proptype=='project'"
-  >
+  <b-row class="mt-2" v-else-if="this.proptype == 'project'">
     <div
       v-for="vote in this.vote"
       :key="vote.projectcc"
@@ -27,13 +17,10 @@
       :style="vote.style"
       @click="voteswitch(vote.projectcc)"
     >
-      <div class="showme mini">{{vote.vlong}} : {{ vote.v }}</div>
+      <div class="showme t16">{{ vote.vlong }} : {{ vote.v }}</div>
     </div>
   </b-row>
-  <b-row
-    class="mt-2"
-    v-else-if="this.proptype=='comment'"
-  >
+  <b-row class="mt-2" v-else-if="this.proptype == 'comment'">
     <div
       v-for="vote in this.vote"
       :key="vote.commentcc"
@@ -41,7 +28,7 @@
       :style="vote.style"
       @click="voteswitch(vote.commentcc)"
     >
-      <div class="showme minimini">{{vote.vlong}} : {{ vote.v }}</div>
+      <div class="showme t10">{{ vote.vlong }} : {{ vote.v }}</div>
     </div>
   </b-row>
 </template>
@@ -56,90 +43,90 @@ export default {
   computed: {
     vote() {
       //need to avoid this to cycle through user votebar, which have always the same color
-      let voteif = [];
+      let voteif = []
       for (let i = 0; i < 7; i++) {
         // choosing the style to apply if the user voted this project/comment specific vote color:
-        var votestyle;
-        var uservoted;
-        this.$store.state[this.proptype + "uservote"]
+        var votestyle
+        var uservoted
+        this.$store.state[this.proptype + 'uservote']
           ? (uservoted = this.$store.state[
-              this.proptype + "uservote"
+              this.proptype + 'uservote'
             ].findIndex(
               x =>
                 x[this.proptype] == this.voteprop.id &&
                 x.condition == this.$store.state.cc[i]
             ))
-          : (uservoted = 0);
+          : (uservoted = 0)
         uservoted != -1
-          ? (votestyle = "var(--my" + this.$store.state.condition[i] + ")")
+          ? (votestyle = 'var(--my' + this.$store.state.condition[i] + ')')
           : this.voteprop[this.$store.state.cc[i]] != 0
-          ? (votestyle = "var(--they" + this.$store.state.condition[i] + ")")
-          : (votestyle = "var(--no" + this.$store.state.condition[i] + ")");
+          ? (votestyle = 'var(--they' + this.$store.state.condition[i] + ')')
+          : (votestyle = 'var(--no' + this.$store.state.condition[i] + ')')
         // applying the right style to vote:
         voteif.push({
           // for all vote:
           v: this.voteprop[this.$store.state.cc[i]],
           vlong: this.$store.state.condition[i],
-          style: "background-color: " + votestyle
-        });
+          style: 'background-color: ' + votestyle
+        })
       } // now we already have a 7 elements array so we just add json keys as needed:
-      if (this.proptype == "user") {
+      if (this.proptype == 'user') {
         for (let i = 0; i < 7; i++) {
-          (voteif[i].class =
-            "uservote b" +
+          ;(voteif[i].class =
+            'uservote b' +
             this.$store.state.condition[i] +
-            " " +
+            ' ' +
             this.$store.state.condition[i]),
             (voteif[i].style =
-              "width: " +
+              'width: ' +
               (this.$auth.user[this.$store.state.cc[i]] / 28) * 100 +
-              "%"),
-            (voteif[i].userkey = this.$store.state.cc[i]);
+              '%'),
+            (voteif[i].userkey = this.$store.state.cc[i])
         }
       }
-      if (this.proptype == "project") {
+      if (this.proptype == 'project') {
         for (let i = 0; i < 7; i++) {
-          (voteif[i].class =
-            "vote col b" +
+          ;(voteif[i].class =
+            'vote col b' +
             this.$store.state.condition[i] +
-            " " +
+            ' ' +
             this.$store.state.condition[i]),
-            (voteif[i].projectcc = this.$store.state.cc[i]);
+            (voteif[i].projectcc = this.$store.state.cc[i])
         }
       }
-      if (this.proptype == "comment") {
+      if (this.proptype == 'comment') {
         for (let i = 0; i < 7; i++) {
-          (voteif[i].class =
-            "vote col b" +
+          ;(voteif[i].class =
+            'vote col b' +
             this.$store.state.condition[i] +
-            " " +
+            ' ' +
             this.$store.state.condition[i]),
-            (voteif[i].commentcc = this.$store.state.cc[i]);
+            (voteif[i].commentcc = this.$store.state.cc[i])
         }
       }
-      return voteif;
+      return voteif
     }
   },
   methods: {
     voteswitch(cc) {
       if (!this.$auth.user) {
-        return this.$router.push({ path: "/login" });
+        return this.$router.push({ path: '/login' })
       }
       // CLIENT-SIDE: sends id, votetype, user and adds 1/-1 to the vuex store variable
-      var add;
-      this.$store.state[this.proptype + "uservote"].findIndex(
+      var add
+      this.$store.state[this.proptype + 'uservote'].findIndex(
         // checks if the vote exists, adds accordingly
         x => x[this.proptype] == this.voteprop.id && x.condition == cc
       ) == -1
         ? (add = 1)
-        : (add = -1);
-      this.$store.commit("setVoteUpdate", {
+        : (add = -1)
+      this.$store.commit('setVoteUpdate', {
         id: this.voteprop.id,
         cc: cc,
         user: this.$auth.user.id,
         add: add,
         proptype: this.proptype
-      });
+      })
 
       // SERVER-SIDE: prepares and sends async REST call (either comment or project)
       var request = {
@@ -147,15 +134,15 @@ export default {
         condition: cc,
         user: this.$auth.user.id,
         proptype: this.proptype
-      };
-      if (this.proptype == "comment") {
-        request.projectid = this.$route.params.id;
-        request.author = this.voteprop.user;
       }
-      this.$store.dispatch("addVoteAction", request).catch(err => {
-        console.error(err);
-      });
+      if (this.proptype == 'comment') {
+        request.projectid = this.$route.params.id
+        request.author = this.voteprop.user
+      }
+      this.$store.dispatch('addVoteAction', request).catch(err => {
+        console.error(err)
+      })
     }
   }
-};
+}
 </script>
--- pages/fun.vue
+++ pages/fun.vue
@@ -1,75 +1,174 @@
 <template>
   <div>
-    <br /><img
-      class="page-header"
-      src="~/assets/image/page/banners/fun.png"
-    />
-    <br>
+    <br /><img class="page-header" src="~/assets/image/page/banners/fun.png" />
+    <br />
     <p class="subheading diversity center space margin">FUN</p>
-    <p class="base">Fun and games make us feel together, especially if the gameplay is not too competitive. Fun enhances the bright side of the artistic zest and the power of creative innovation. Art brings our <a
-        class="ad"
-        href="/cooperation#diversity"
-      >diversity</a> to become beauty. Art, fun, games and innovation represent the generative side of our lives. This page is dedicated to fun, creativity and cooperation.</p>
-
-    <br>
-    <p class="subheading diversity center space margin">BUILD YOUR OWN PINWHEEL GALAXY</p>
     <p class="base">
-      Making a pinwheel is very nice and you can do this little do-it-yourself craft with your friends. The Pinwheel Galaxy is a spiral-shaped galaxy about 21 million light years away from Earth. The first Cooperacy logo was a colorful pinwheel and that idea still lives in the current rotating rainbow circle logo in the homepage. The Cooperacy members chose the pinwheel as the symbol of cooperation because whe the diversity of the colors spins, it becomes a common, intense, white light. Celebrate cooperation blowing on a pinwheel made of stars! To build it, you can follow the instructions carefully put together by <a
-        class="au"
-        href="https://spaceplace.nasa.gov/pinwheel-galaxy/en/"
-      >NASA</a>.<br> After you made one, try and make a 7 petals pinwheel with no red, like the Cooperacy original logo!</p>
+      Fun and games make us feel together, especially if the gameplay is not too
+      competitive. Fun enhances the bright side of the artistic zest and the
+      power of creative innovation. Art brings our
+      <a class="ad" href="/cooperation#diversity">diversity</a> to become
+      beauty. Art, fun, games and innovation represent the generative side of
+      our lives. This page is dedicated to fun, creativity and cooperation.
+    </p>
+
+    <br />
+    <p class="subheading diversity center space margin">
+      BUILD YOUR OWN PINWHEEL GALAXY
+    </p>
+    <p class="base">
+      Making a pinwheel is very nice and you can do this little do-it-yourself
+      craft with your friends. The Pinwheel Galaxy is a spiral-shaped galaxy
+      about 21 million light years away from Earth. The first Cooperacy logo was
+      a colorful pinwheel and that idea still lives in the current rotating
+      rainbow circle logo in the homepage. The Cooperacy members chose the
+      pinwheel as the symbol of cooperation because whe the diversity of the
+      colors spins, it becomes a common, intense, white light. Celebrate
+      cooperation blowing on a pinwheel made of stars! To build it, you can
+      follow the instructions carefully put together by
+      <a class="au" href="https://spaceplace.nasa.gov/pinwheel-galaxy/en/"
+        >NASA</a
+      >.<br />
+      After you made one, try and make a 7 petals pinwheel with no red, like the
+      Cooperacy original logo!
+    </p>
 
     <!--BEGIN WHEEL-->
     <div style="position: relative; text-align: center;">
-      <div
-        class="rotating"
-        style="position: absolute; z-index: 1;"
-      ><img
+      <div class="rotating" style="position: absolute; z-index: 1;">
+        <img
           src="~/assets/image/page/fun/pinwheel.png"
           width="330"
           height="330"
-        ></div>
-      <div style="position: absolute; z-index: 2"><img src="~/assets/image/page/fun/typo.png"></div>
-      <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        />
+      </div>
+
+      <div style="position: absolute; z-index: 2">
+        <img src="~/assets/image/page/fun/typo.png" />
+      </div>
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
     </div>
     <!--END WHEEL-->
 
-    <br>
+    <br />
     <p class="subheading diversity center space margin">COOPERATIVE MONOPOLY</p>
-    <p class="base">Cooperative Monopoly is a game that can be played with a standard monopoly set to understand the economic system we live in nowadays. Following these simple steps, you may experience a transition from the standard competitive economy to a different one based on cooperation.<br /><br />
-      We recently discovered Monopoly was invented by <a
-        class="ad"
-        rc="https://en.wikipedia.org/wiki/Lizzie_Magie"
-      >Lizzy Magie</a>, a <a
-        class="au"
-        src="https://en.wikipedia.org/wiki/Georgism"
-      >Georgist</a> and game designer from the US. The game was called "The Landlord Game" or "Prosperity". The two names depend on the fact the original game had two set of rules, one quite similar to the Monopoly ones (The Landlord), the other one based on the idea that every land was owned by a collective treasury that wouldn't exclude anyone from being eliminated from the game (Prosperity).<br /><br />
-      We tested the "Prosperity" set of rules and unfortunately, although they are better than the competitive ones, they do not reach a full balanced game. We suggest you to try the cooperative monopoly narrative, and see what happens in your case:<br /></p>
-    <p class="diversity center space"> NARRATIVE ONE</p>
-    <p class="mini just">A quick way to experience a cooperative social ecosystem is to play the Monopoly game with this different mechanism:<br>
-      Every time a player steps on an owned property, with or without buildings, the property produces money FROM the Monopoly box (the bank) TO the owner, both if the player on the property is the same owner and if it is another player. In practice, the different properties become productive entities, and any player who passes by generates wealth for the city.<br>
-      The difficult part is to face the costs of the city maintainance, that may be accounted individually or colletively.<br>
-      In the individual agreement, every turn each player has to pay 50$ for each property he owns (if you don't have dollars, choose the third banknote amount the game box provides, for example, if you have 50.000, 10.000, 5.000, 2.000, etc. you choose 5.000).<br>
-      In the collective agreement, the players should pay an amount that starts from 50$ but making it grow by 50$ every full turn (turn 1: 50$ each, turn 2: 100$ each, turn 3: 150$ each).<br>
-      Independently of the agreement, the players lose if they don't have any more properties and win when they managed instead to collectively own all the properties and all the money of the game.<br></p>
-    <p class="diversity center space"> NARRATIVE TWO</p>
-    <p class="mini just"><span class="diversity">1.</span>This narrative is more open to interpretations and starts from the standard rules of the game to pass through transformations that allow the players to note the difference between a competitive system to a cooperative one.<br>
-      When the game starts, every participant receives 12 houses that can be set up anytime in any land with no cost. The game proceeds as it normally would in a standard Monopoly game.<br />
-      <span class="diversity">2.</span> When the first player runs out of the highest-value banknote type, the <i>participated governance</i> phase begins. In this phase, players can talk to each other with the purpose of forming alliances, sharing ownerships, changing the rules of the game. They can take participated decisions each time it's the poorest player's turn.<br />
-      <span class="diversity">3.</span> When two players decide to put in common some properties or their money, the game enters the <i>collaboration</i> phase. Players may even merge in teams and play one team against the other(s). The term collaboration indicates a general form of interaction as the coalition is not neutral. Sometimes strong competitive players get isolated until their approach changes.<br />
-      <span class="diversity">4.</span> It can happen that players decide to redistribute the money owned by each of them, and the game gets into the <i>redistribution</i> phase. The players usually change the objective of the game or insert an agreement that nobody can go bankrupt and if it happens should be saved by the others. Then, half of any rent payed goes to the owner, and half to the center of the board. When any banknote type reaches the same number of the players, the banknotes are redistributed.<br />
-      <span class="diversity">5.</span> Another possible step is that players decide to take all the money from the bank, the game evolves into the <i>bank off</i> phase. Sometimes all the money from the bank is redistributed, sometimes a joint team of all the players decides to get the money from the bank and set the objective to build the maximum amount of hotels and houses possible as a final victory of the whole group.<br />
-      <span class="diversity">6.</span> When three rounds have been played in the bank off mode, a coup d’etat is carried out and a <i>dictatorship</i> is established. A depressed tyrant takes all the lands, hotels, houses and overrule the agreements the players might have drawn up, but takes no money. The players have two opportunities now. They continue playing as usual but, when they arrive on a land, they can buy it back paying first the standard rent (which is not redistributed), and then paying the value of the land and the buildings multiplied by five. This way, they may try to re-buy their city. The other opportunity is to make the depressed leader leave, that is, to make him happy again. His will is to receive all the players money, in order to accomplish a secret goal of his (going to Mars, building a mausoleum made of gold, etc.). The players could therefore also decide to give all the money they have to the dictator and make him happy. When the dictator is happy, he will go away with the money and leave the city to all the players.<br />
-      <span class="diversity">7.</span> When the depressed dictator goes away, the <i>creativity</i> final phase can be started: the whole city is owned by the participants and is in common. The players are free to imagine a new city that works differently, every one of them may choose their area and color of concern, keeping in mind the city needs energy, water, food production, and the like. Every color may be dedicated to a need, a service or an activity. The city is free to imagine a new social ecosystem based on the beliefs and ideas of the players!<br>
-      <br>
-      These are two example narratives, but of course you can set your own and enjoy your city!
-    </p><br>
+    <p class="base">
+      Cooperative Monopoly is a game that can be played with a standard monopoly
+      set to understand the economic system we live in nowadays. Following these
+      simple steps, you may experience a transition from the standard
+      competitive economy to a different one based on cooperation.<br /><br />
+      We recently discovered Monopoly was invented by
+      <a class="ad" rc="https://en.wikipedia.org/wiki/Lizzie_Magie"
+        >Lizzy Magie</a
+      >, a
+      <a class="au" src="https://en.wikipedia.org/wiki/Georgism">Georgist</a>
+      and game designer from the US. The game was called "The Landlord Game" or
+      "Prosperity". The two names depend on the fact the original game had two
+      set of rules, one quite similar to the Monopoly ones (The Landlord), the
+      other one based on the idea that every land was owned by a collective
+      treasury that wouldn't exclude anyone from being eliminated from the game
+      (Prosperity).<br /><br />
+      We tested the "Prosperity" set of rules and unfortunately, although they
+      are better than the competitive ones, they do not reach a full balanced
+      game. We suggest you to try the cooperative monopoly narrative, and see
+      what happens in your case:<br />
+    </p>
+    <p class="diversity center space">NARRATIVE ONE</p>
+    <p class="t16 just">
+      A quick way to experience a cooperative social ecosystem is to play the
+      Monopoly game with this different mechanism:<br />
+      Every time a player steps on an owned property, with or without buildings,
+      the property produces money FROM the Monopoly box (the bank) TO the owner,
+      both if the player on the property is the same owner and if it is another
+      player. In practice, the different properties become productive entities,
+      and any player who passes by generates wealth for the city.<br />
+      The difficult part is to face the costs of the city maintainance, that may
+      be accounted individually or colletively.<br />
+      In the individual agreement, every turn each player has to pay 50$ for
+      each property he owns (if you don't have dollars, choose the third
+      banknote amount the game box provides, for example, if you have 50.000,
+      10.000, 5.000, 2.000, etc. you choose 5.000).<br />
+      In the collective agreement, the players should pay an amount that starts
+      from 50$ but making it grow by 50$ every full turn (turn 1: 50$ each, turn
+      2: 100$ each, turn 3: 150$ each).<br />
+      Independently of the agreement, the players lose if they don't have any
+      more properties and win when they managed instead to collectively own all
+      the properties and all the money of the game.<br />
+    </p>
 
-    <br>
+    <p class="diversity center space">NARRATIVE TWO</p>
+    <p class="t16 just">
+      <span class="diversity">1.</span>This narrative is more open to
+      interpretations and starts from the standard rules of the game to pass
+      through transformations that allow the players to note the difference
+      between a competitive system to a cooperative one.<br />
+      When the game starts, every participant receives 12 houses that can be set
+      up anytime in any land with no cost. The game proceeds as it normally
+      would in a standard Monopoly game.<br />
+      <span class="diversity">2.</span> When the first player runs out of the
+      highest-value banknote type, the <i>participated governance</i> phase
+      begins. In this phase, players can talk to each other with the purpose of
+      forming alliances, sharing ownerships, changing the rules of the game.
+      They can take participated decisions each time it's the poorest player's
+      turn.<br />
+      <span class="diversity">3.</span> When two players decide to put in common
+      some properties or their money, the game enters the
+      <i>collaboration</i> phase. Players may even merge in teams and play one
+      team against the other(s). The term collaboration indicates a general form
+      of interaction as the coalition is not neutral. Sometimes strong
+      competitive players get isolated until their approach changes.<br />
+      <span class="diversity">4.</span> It can happen that players decide to
+      redistribute the money owned by each of them, and the game gets into the
+      <i>redistribution</i> phase. The players usually change the objective of
+      the game or insert an agreement that nobody can go bankrupt and if it
+      happens should be saved by the others. Then, half of any rent payed goes
+      to the owner, and half to the center of the board. When any banknote type
+      reaches the same number of the players, the banknotes are
+      redistributed.<br />
+      <span class="diversity">5.</span> Another possible step is that players
+      decide to take all the money from the bank, the game evolves into the
+      <i>bank off</i> phase. Sometimes all the money from the bank is
+      redistributed, sometimes a joint team of all the players decides to get
+      the money from the bank and set the objective to build the maximum amount
+      of hotels and houses possible as a final victory of the whole group.<br />
+      <span class="diversity">6.</span> When three rounds have been played in
+      the bank off mode, a coup d’etat is carried out and a
+      <i>dictatorship</i> is established. A depressed tyrant takes all the
+      lands, hotels, houses and overrule the agreements the players might have
+      drawn up, but takes no money. The players have two opportunities now. They
+      continue playing as usual but, when they arrive on a land, they can buy it
+      back paying first the standard rent (which is not redistributed), and then
+      paying the value of the land and the buildings multiplied by five. This
+      way, they may try to re-buy their city. The other opportunity is to make
+      the depressed leader leave, that is, to make him happy again. His will is
+      to receive all the players money, in order to accomplish a secret goal of
+      his (going to Mars, building a mausoleum made of gold, etc.). The players
+      could therefore also decide to give all the money they have to the
+      dictator and make him happy. When the dictator is happy, he will go away
+      with the money and leave the city to all the players.<br />
+      <span class="diversity">7.</span> When the depressed dictator goes away,
+      the <i>creativity</i> final phase can be started: the whole city is owned
+      by the participants and is in common. The players are free to imagine a
+      new city that works differently, every one of them may choose their area
+      and color of concern, keeping in mind the city needs energy, water, food
+      production, and the like. Every color may be dedicated to a need, a
+      service or an activity. The city is free to imagine a new social ecosystem
+      based on the beliefs and ideas of the players!<br />
+      <br />
+      These are two example narratives, but of course you can set your own and
+      enjoy your city!
+    </p>
+    <br />
+
+    <br />
     <p class="subheading diversity center space margin">COOPERATON AND FUN</p>
-    <p class="base">You may also imagine new gamifications based on a cooperation-based gameplay, or structured with an ecosystemic thinking approach. To learn more about the possibilities of integrating cooperation in your dynamics or your narratives, have a look at our <a
-        class="au"
-        href="/cooperation"
-      >learning area.</a></p><br><br><br>
+    <p class="base">
+      You may also imagine new gamifications based on a cooperation-based
+      gameplay, or structured with an ecosystemic thinking approach. To learn
+      more about the possibilities of integrating cooperation in your dynamics
+      or your narratives, have a look at our
+      <a class="au" href="/cooperation">learning area.</a>
+    </p>
+    <br /><br /><br />
   </div>
 </template>
