c3d7690a36ba7c57bd3ac9f32a0483b11edd9fd6
Alessandro Merletti De Palo <alessandromerlettidepalo@gmail.com>
2019-08-25 21:44:07 +0200

    new menu layout, new font css names

--- components/foot.vue
+++ components/foot.vue
@@ -1,33 +1,31 @@
 <template>
   <div>
-    <div class="foot row d-flex justify-content-center mini mt-5">
+    <div class="foot row d-flex justify-content-center t16 mt-5">
       <p class="base space my-2">
-        <a
-          to="/login"
-          class="ae"
-          style="background-image:none"
-        >JOIN</a> -
+        <a to="/login" class="ae" style="background-image:none">JOIN</a> -
         <a
           class="af"
           style="background-image:none"
           href="https://m.me/cooperacy.org"
-        >CHAT</a> -
+          >CHAT</a
+        >
+        -
         <a
           href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=YS7MHXBSVDDEU"
           class="at"
           style="background-image:none"
-        >DONATE</a>
+          >DONATE</a
+        >
       </p>
-      <p class="mini space m-0">&copy; COOPERACY RESEARCH ASSOCIATION 2015-2018 - VIA DELLA MOSCOVA, 39 - 20121 MILAN ITALY EU - FISCAL CODE 97725540153</p>
-      <p class="mini space mb-2">
-        <nuxt-link
-          class="ai"
-          to="/terms"
-        >PRIVACY POLICY AND AGREEMENTS</nuxt-link>
-        <nuxt-link
-          class="ai"
-          to="/contact"
-        >CONTACT</nuxt-link>
+      <p class="t16 space m-0">
+        &copy; COOPERACY RESEARCH ASSOCIATION 2015-2018 - VIA DELLA MOSCOVA, 39
+        - 20121 MILAN ITALY EU - FISCAL CODE 97725540153
+      </p>
+      <p class="t16 space mb-2">
+        <nuxt-link class="ai" to="/terms"
+          >PRIVACY POLICY AND AGREEMENTS</nuxt-link
+        >
+        <nuxt-link class="ai" to="/contact">CONTACT</nuxt-link>
       </p>
     </div>
   </div>
--- components/navbar.vue
+++ components/navbar.vue
@@ -1,162 +1,206 @@
 <template>
-  <b-navbar
-    toggleable="lg"
-    class="fixed-top bg-white d-flex flex-fill justify-content-center align-middle p-0"
-  >
-    <b-row class="w-100">
-      <b-col cols="4 d-flex">
-        <!-- logo -->
-        <b-navbar-brand class="headerlogo mt-1" to="/"
-          ><img class="headerlogo" src="@/assets/image/main/wordlogo.png" />
-        </b-navbar-brand>
-      </b-col>
-
-      <b-navbar-toggle target=".nav-collapse"></b-navbar-toggle>
-
-      <b-col cols="4 d-flex">
-        <b-collapse class="nav-collapse" is-nav>
-          <!--menu-->
-          <div class="container d-flex justify-content-center">
-            <b-navbar-nav class="minini">
-              <b-nav-item-dropdown text="THE PROJECT" no-caret center
-                ><b-dropdown-item>
-                  <nuxt-link to="/aboutyou"
-                    >ABOUT YOU &nbsp;&nbsp;&nbsp;</nuxt-link
-                  >
-
-                  <nuxt-link to="/aboutus"
-                    >ABOUT US &nbsp;&nbsp;&nbsp;</nuxt-link
-                  >
-
-                  <nuxt-link to="/platform"
-                    >THE PLATFORM &nbsp;&nbsp;&nbsp;</nuxt-link
-                  >
-
-                  <nuxt-link to="/governance"
-                    >GOVERNANCE &nbsp;&nbsp;&nbsp;</nuxt-link
-                  >
-                </b-dropdown-item>
-              </b-nav-item-dropdown>
-
-              <b-nav-item-dropdown text="CORPORATE" no-caret right>
-                <b-dropdown-item>
-                  <nuxt-link to="/corporate"
-                    >CORPORATE WORLD &nbsp;&nbsp;&nbsp;</nuxt-link
-                  >
-
-                  <nuxt-link to="/overview"
-                    >MEASURES &nbsp;&nbsp;&nbsp;</nuxt-link
-                  >
-
-                  <nuxt-link to="/coopenspace"
-                    >CO-OPEN SPACE &nbsp;&nbsp;&nbsp;</nuxt-link
-                  >
-
-                  <nuxt-link to="/cohackathon"
-                    >CO-HACKATHON &nbsp;&nbsp;&nbsp;</nuxt-link
-                  >
-
-                  <nuxt-link to="/workshops"
-                    >WORKSHOPS &nbsp;&nbsp;&nbsp;</nuxt-link
-                  >
-                </b-dropdown-item>
-              </b-nav-item-dropdown>
-
-              <b-nav-item-dropdown text="SCIENCE" no-caret right>
-                <b-dropdown-item>
-                  <nuxt-link to="/cooperation"
-                    >COOPERATION &nbsp;&nbsp;&nbsp;</nuxt-link
-                  >
-
-                  <nuxt-link to="/integration">
-                    INTEGRATION &nbsp;&nbsp;&nbsp;</nuxt-link
-                  >
-
-                  <nuxt-link to="/cci"> CCI &nbsp;&nbsp;&nbsp;</nuxt-link>
-
-                  <nuxt-link to="/science">
-                    CRA &nbsp;&nbsp;&nbsp;
-                  </nuxt-link>
-
-                  <nuxt-link to="/news"> NEWS</nuxt-link>
-                </b-dropdown-item>
-              </b-nav-item-dropdown>
-            </b-navbar-nav>
+  <div class="container m-0 p-0">
+    <div class="fixed-top bg-white p-0 d-none d-lg-block">
+      <b-row class="w-100 m-0">
+        <b-col cols="2" class="p-0">
+          <!-- logo -->
+          <div class="container headerlogo p-0">
+            <nuxt-link to="/" tag="img" class="imglogo" :src="wordLogo">
+            </nuxt-link>
           </div>
-          <!-- end menu -->
-        </b-collapse>
-      </b-col>
-
-      <b-col cols="3 d-flex p-0">
-        <b-collapse class="nav-collapse" is-nav>
+        </b-col>
+        <b-col cols="8" class="p-0 d-flex">
+          <!--menu-->
+          <b-row class="w-100 m-0 upmenu">
+            <b-col cols="12" class="p-0 d-flex">
+              <div class="container d-flex justify-content-center p-0 t14">
+                <div @mouseover="menushow('project')" class="pr-4">
+                  THE PROJECT
+                </div>
+                <div @mouseover="menushow('corporate')" class="pr-4">
+                  CORPORATE
+                </div>
+                <div @mouseover="menushow('science')">
+                  SCIENCE
+                </div>
+              </div>
+            </b-col>
+            <b-col cols="12" class="d-flex">
+              <div
+                class="container d-flex justify-content-center submenu"
+                v-if="coomenus.project"
+              >
+                <nuxt-link class="mr-3" to="/aboutyou">ABOUT YOU</nuxt-link>
+                <nuxt-link class="mr-3" to="/aboutus">ABOUT US</nuxt-link>
+                <nuxt-link class="mr-3" to="/platform">THE PLATFORM</nuxt-link>
+                <nuxt-link class="mr-3" to="/governance"> GOVERNANCE</nuxt-link>
+              </div>
+              <div
+                class="container d-flex justify-content-center submenu"
+                v-if="coomenus.corporate"
+              >
+                <nuxt-link class="mr-3" to="/corporate">
+                  CORPORATE WORLD
+                </nuxt-link>
+                <nuxt-link class="mr-3" to="/overview">
+                  MEASURING COOPERATION
+                </nuxt-link>
+                <nuxt-link class="mr-3" to="/coopenspace">
+                  CO-OPEN SPACE
+                </nuxt-link>
+                <nuxt-link class="mr-3" to="/cohackathon">
+                  CO-HACKATHON
+                </nuxt-link>
+                <nuxt-link class="mr-3" to="/workshops">
+                  WORKSHOPS
+                </nuxt-link>
+              </div>
+              <div
+                class="container d-flex justify-content-center submenu"
+                v-if="coomenus.science"
+              >
+                <nuxt-link class="mr-3" to="/cooperation">
+                  COOPERATION
+                </nuxt-link>
+                <nuxt-link class="mr-3" to="/integration">
+                  INTEGRATION
+                </nuxt-link>
+                <nuxt-link class="mr-3" to="/cci">
+                  COOPERATION CONTEXT INDEX
+                </nuxt-link>
+                <nuxt-link class="mr-3" to="/science">
+                  COOPERACY RESEARCH ASSOCIATION
+                </nuxt-link>
+                <nuxt-link class="mr-3" to="/news">
+                  NEWS
+                </nuxt-link>
+              </div>
+            </b-col>
+          </b-row>
+        </b-col>
+        <b-col cols="1" class="p-0 d-flex">
           <!-- user name and surname -->
           <div
             class="container d-flex justify-content-end p-0"
             v-if="this.$auth.loggedIn"
           >
-            <b-navbar-nav class="up minini">
+            <b-navbar-nav class="up t12">
               <b-nav-text class="align-middle">
                 {{ this.$auth.user.name + ' ' + this.$auth.user.surname }}
               </b-nav-text>
             </b-navbar-nav>
           </div>
-        </b-collapse>
-      </b-col>
-      <b-col cols="1 d-flex">
-        <b-collapse class="nav-collapse" is-nav>
+        </b-col>
+        <b-col cols="1" class="p-0 d-flex">
           <!-- login -->
           <div
             class="container d-flex justify-content-end"
             v-if="!this.$auth.loggedIn"
           >
-            <b-navbar-nav class="up minini">
+            <b-navbar-nav class="up t14">
               <b-nav-item to="/login">LOGIN</b-nav-item>
             </b-navbar-nav>
           </div>
+          <!-- user image link -->
           <div
-            class="container d-flex justify-content-end"
+            class="container d-flex justify-content-center p-0"
             v-if="this.$auth.loggedIn"
           >
-            <b-navbar-nav @click="show = !show">
-              <!-- logout dropdown -->
-              <img
-                class="usercoologo m-2 p-0"
-                src="@/assets/image/main/coo.png"
-              />
-              <img
-                v-if="userImage"
-                class="userlogo rounded-circle m-2 p-0"
-                :src="userImage"
-              />
-              <div
-                class="minimini hideover"
-                @click="$auth.logout()"
-                v-if="show"
-              >
-                LOGOUT
-              </div>
-            </b-navbar-nav>
+            <nuxt-link
+              to="/user"
+              tag="img"
+              class="userlogo rounded-circle"
+              :src="userImage"
+            >
+            </nuxt-link>
           </div>
-        </b-collapse>
-      </b-col>
-      <b-col
-        v-if="this.$auth.loggedIn"
-        cols="12"
-        class="p-0"
-        style="margin-bottom: 2px"
-      >
-        <votebar :voteprop="$auth.user" :proptype="'user'" />
-      </b-col>
+        </b-col>
+        <b-col v-if="this.$auth.loggedIn" cols="12" class="p-0 d-flex">
+          <votebar :voteprop="$auth.user" :proptype="'user'" />
+        </b-col>
 
-      <b-col
-        v-if="!this.$auth.loggedIn"
-        cols="12"
-        class="p-0 bg-secondary"
-        style="height: 1px"
-        >&nbsp;
-      </b-col>
-    </b-row>
-  </b-navbar>
+        <b-col
+          v-if="!this.$auth.loggedIn"
+          cols="12"
+          class="p-0 bg-secondary"
+          style="height: 1px"
+          >&nbsp;
+        </b-col>
+      </b-row>
+    </div>
+
+    <!---------------------------- mobile ---------------------------->
+    <b-navbar toggleable="lg" class="fixed-top bg-white p-0 d-lg-none">
+      <div class="container m-0 p-0">
+        <b-row class="w-100 m-0 rebar">
+          <b-col cols="3" class="float-left">
+            <b-navbar-brand class="headerlogo" to="/">
+              <img class="headerlogo" src="@/assets/image/main/wordlogo.png" />
+            </b-navbar-brand>
+          </b-col>
+          <b-col cols="9" class="p-0 float-right">
+            <b-navbar-toggle
+              target="nav-collapse"
+              class="float-right"
+            ></b-navbar-toggle>
+            <b-collapse id="nav-collapse" is-nav>
+              <b-navbar-nav class="t12 float-right">
+                <b-nav-dropdown text="THE PROJECT">
+                  <b-dropdown-item to="/aboutyou" class="t12">
+                    ABOUT YOU
+                  </b-dropdown-item>
+                  <b-dropdown-item to="/aboutus" class="t12">
+                    ABOUT US
+                  </b-dropdown-item>
+                  <b-dropdown-item to="/platform" class="t12">
+                    THE PLATFORM
+                  </b-dropdown-item>
+                  <b-dropdown-item to="/governance" class="t12">
+                    GOVERNANCE
+                  </b-dropdown-item>
+                </b-nav-dropdown>
+
+                <b-nav-dropdown text="CORPORATE">
+                  <b-dropdown-item to="/corporate" class="t12">
+                    CORPORATE WORLD
+                  </b-dropdown-item>
+                  <b-dropdown-item to="/overview" class="t12">
+                    MEASURING COOPERATION
+                  </b-dropdown-item>
+                  <b-dropdown-item to="/coopenspace" class="t12">
+                    CO-OPEN SPACE
+                  </b-dropdown-item>
+                  <b-dropdown-item to="/cohackathon" class="t12">
+                    CO-HACKATHON
+                  </b-dropdown-item>
+                  <b-dropdown-item to="/workshops" class="t12">
+                    WORKSHOPS
+                  </b-dropdown-item>
+                </b-nav-dropdown>
+                <b-nav-dropdown text="SCIENCE">
+                  <b-dropdown-item to="/cooperation" class="t12">
+                    COOPERATION
+                  </b-dropdown-item>
+                  <b-dropdown-item to="/integration" class="t12">
+                    INTEGRATION
+                  </b-dropdown-item>
+                  <b-dropdown-item to="/cci" class="t12">
+                    COOPERATION CONTEXT INDEX
+                  </b-dropdown-item>
+                  <b-dropdown-item to="/science" class="t12">
+                    COOPERACY RESEARCH ASSOCIATION
+                  </b-dropdown-item>
+                  <b-dropdown-item to="/news" class="t12">
+                    NEWS
+                  </b-dropdown-item>
+                </b-nav-dropdown>
+              </b-navbar-nav>
+            </b-collapse>
+          </b-col>
+        </b-row>
+      </div>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
@@ -164,7 +208,9 @@ import votebar from './votebar'
 
 export default {
   data() {
-    return { show: false }
+    return {
+      coomenus: { project: false, corporate: false, science: false }
+    }
   },
   components: { votebar: votebar },
   computed: {
@@ -172,8 +218,23 @@ export default {
       try {
         return require('../assets/image/user/' + this.$auth.user.id + '.png')
       } catch (e) {
-        return
+        return require('../assets/image/main/coo.png')
       }
+    },
+    wordLogo() {
+      try {
+        return require('../assets/image/main/wordlogo.png')
+      } catch (e) {
+        return 'not found'
+      }
+    }
+  },
+  methods: {
+    menushow(menu) {
+      this.coomenus['project'] = false
+      this.coomenus['corporate'] = false
+      this.coomenus['science'] = false
+      this.coomenus[menu] = true
     }
   }
 }
