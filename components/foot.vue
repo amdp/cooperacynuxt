469032de01ab71327b1af85332eba5d0e6c8f4b9
Alessandro Merletti De Palo <alessandromerlettidepalo@gmail.com>
2019-08-28 13:00:23 +0200

    better layout + footer + cookies alert

--- components/foot.vue
+++ components/foot.vue
@@ -1,32 +1,45 @@
 <template>
-  <div>
-    <div class="foot row d-flex justify-content-center t16 mt-5">
-      <p class="base space my-2">
-        <a to="/login" class="ae" style="background-image:none">JOIN</a> -
-        <a
-          class="af"
-          style="background-image:none"
-          href="https://m.me/cooperacy.org"
-          >CHAT</a
-        >
-        -
-        <a
-          href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=YS7MHXBSVDDEU"
-          class="at"
-          style="background-image:none"
-          >DONATE</a
-        >
-      </p>
-      <p class="t16 space m-0">
-        &copy; COOPERACY RESEARCH ASSOCIATION 2015-2018 - VIA DELLA MOSCOVA, 39
-        - 20121 MILAN ITALY EU - FISCAL CODE 97725540153
-      </p>
-      <p class="t16 space mb-2">
-        <nuxt-link class="ai" to="/terms"
-          >PRIVACY POLICY AND AGREEMENTS</nuxt-link
-        >
-        <nuxt-link class="ai" to="/contact">CONTACT</nuxt-link>
-      </p>
-    </div>
+  <div class="container-fluid p-0">
+    <b-row>
+      <div class="container-fluid foot d-flex justify-content-center t16 m-0">
+        <p class="base space my-2">
+          <a to="/login" class="ae" style="background-image:none">JOIN</a> -
+          <a
+            class="af"
+            style="background-image:none"
+            href="https://m.me/cooperacy.org"
+            >CHAT</a
+          >
+          -
+          <a
+            href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=YS7MHXBSVDDEU"
+            class="at"
+            style="background-image:none"
+            >DONATE</a
+          >
+          -
+          <a class="ai" style="background-image:none" to="/contact">CONTACT</a>
+        </p>
+      </div>
+    </b-row>
+    <b-row>
+      <div class="container-fluid d-flex justify-content-center t16">
+        <p class="t16 space m-0 text-center pl-4 pr-4">
+          &copy; COOPERACY RESEARCH ASSOCIATION 2015-2018 - VIA DELLA MOSCOVA,
+          39 - 20121 MILAN ITALY EU - FISCAL CODE 97725540153
+        </p>
+      </div>
+    </b-row>
+    <b-row>
+      <div class="container-fluid d-flex justify-content-center t16">
+        <p class="t16 space mb-2 text-center pl-4 pr-4">
+          <em><strong>WE USE COOKIES</strong></em
+          >: BY VISITING THIS SITE YOU ABIDE TO OUR
+          <nuxt-link class="ai" to="/terms">
+            PRIVACY POLICY AND AGREEMENTS</nuxt-link
+          >
+        </p>
+      </div>
+    </b-row>
   </div>
 </template>
