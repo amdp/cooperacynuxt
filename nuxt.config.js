1c86a3cbce3648c6afcbf4f908fd02debd868e0c
Alessandro Merletti De Palo <alessandromerlettidepalo@gmail.com>
2019-08-30 20:57:30 +0200

    fixed font in build

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,14 @@ module.exports = {
       },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Prompt:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap'
+      }
+    ]
   },
 
   auth: {
