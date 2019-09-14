53c55dcbf79694e9c44265e12aded9fc8bc0e0ec
Ali Akhtari <aaliiaakhtarii@gmail.com>
2019-09-14 10:55:31 +0430

    updated img.page-header to be responsive

--- pages/platform.vue
+++ pages/platform.vue
@@ -1,49 +1,82 @@
 <template>
-  <div>
+  <div class="page-container">
     <br /><img
       class="page-header"
       src="~/assets/image/page/banners/platform.png"
     />
 
     <p class="subheading transparency center space">IMAGINE</p>
-    <p class="base">Imagine the effect of one million people bringing together a little economic effort: one euro. Imagine you and all the other people to reciprocally collect one million euros every month to support your ideas. With this collective fund you will generate an innovative economy in which the more people will participate, the more the people's ideas will be realized.<br></p>
+    <p class="base">
+      Imagine the effect of one million people bringing together a little
+      economic effort: one euro. Imagine you and all the other people to
+      reciprocally collect one million euros every month to support your ideas.
+      With this collective fund you will generate an innovative economy in which
+      the more people will participate, the more the people's ideas will be
+      realized.<br />
+    </p>
 
     <p class="subheading transparency center space">CO-OWNING A PLATFORM</p>
-    <p class="base">The Cooperacy Platform won't just realise the most desired ideas proposed by the participants, but also create a social ecosystem based on co-ownership and redistribution of wealth. Every participants will in facts be co-owner of all the platform achievements and funds.</p>
+    <p class="base">
+      The Cooperacy Platform won't just realise the most desired ideas proposed
+      by the participants, but also create a social ecosystem based on
+      co-ownership and redistribution of wealth. Every participants will in
+      facts be co-owner of all the platform achievements and funds.
+    </p>
 
     <p class="subheading center space transparency">VOTING IDEAS</p>
-    <p class="base">The platform distributes the common money and resources to the ideas according to how much they are desired by the participants. Expressing their <a
-        class="ae"
-        href="/cooperation#diversity"
-      >preferences</a> through <a
-        class="ae"
-        href="/cooperation#diversity"
-      >transparent</a> votes, the common collected money goes <a
-        class="ae"
-        href="/integration"
-      >proportionally</a> to the ideas according to how many votes they received. Keeping it simple: people propose <i>ideas</i>, <i>votes</i> allocate the common money.</p>
+    <p class="base">
+      The platform distributes the common money and resources to the ideas
+      according to how much they are desired by the participants. Expressing
+      their <a class="ae" href="/cooperation#diversity">preferences</a> through
+      <a class="ae" href="/cooperation#diversity">transparent</a> votes, the
+      common collected money goes
+      <a class="ae" href="/integration">proportionally</a> to the ideas
+      according to how many votes they received. Keeping it simple: people
+      propose <i>ideas</i>, <i>votes</i> allocate the common money.
+    </p>
 
     <p class="subheading center space transparency">REDISTRIBUTING PROFITS</p>
-    <p class="base">Every idea is co-owned, so when it produces wealth, its profits go back to the platform collected money, in order to fund other ideas and to create a virtuous circle that slowly generates resources for <a
-        class="ae"
-        href="/cooperation#equivalence"
-      >everyone</a>.</p>
+    <p class="base">
+      Every idea is co-owned, so when it produces wealth, its profits go back to
+      the platform collected money, in order to fund other ideas and to create a
+      virtuous circle that slowly generates resources for
+      <a class="ae" href="/cooperation#equivalence">everyone</a>.
+    </p>
 
-    <p class="subheading center space equivalence margin">A COOPERATION LEARNING ECOSYSTEM</p>
-    <p class="base">An artificial intelligence will learn from all the <a
-        class="au"
-        href="/science"
-      >data</a> coming from the platform and suggest, in the long run, best practices in order to help the people to realise their ideas. Moreover, the participants can communicate their happiness, so that the "deep learning ecosystem" will base its suggestions not only on data, but also on the most human, irrational and personal information of all: mood.</p>
+    <p class="subheading center space equivalence margin">
+      A COOPERATION LEARNING ECOSYSTEM
+    </p>
+    <p class="base">
+      An artificial intelligence will learn from all the
+      <a class="au" href="/science">data</a> coming from the platform and
+      suggest, in the long run, best practices in order to help the people to
+      realise their ideas. Moreover, the participants can communicate their
+      happiness, so that the "deep learning ecosystem" will base its suggestions
+      not only on data, but also on the most human, irrational and personal
+      information of all: mood.
+    </p>
 
-    <p class="base"><i>For more technical information about the platform, see its <a
-          class="au"
-          href="/whitepaper"
-        >white paper</a>.</i></p>
+    <p class="base">
+      <i
+        >For more technical information about the platform, see its
+        <a class="au" href="/whitepaper">white paper</a>.</i
+      >
+    </p>
 
-    <p class="base">We would be very happy to see you on board. One euro per month is really a small effort to see how much can be done when we join our forces.<br><br>
-      Enter the <a
-        class="ae"
-        href="/register"
-      >Cooperacy</a> Platform.. let's start doing things together!</p>
+    <p class="base">
+      We would be very happy to see you on board. One euro per month is really a
+      small effort to see how much can be done when we join our forces.<br /><br />
+      Enter the <a class="ae" href="/register">Cooperacy</a> Platform.. let's
+      start doing things together!
+    </p>
   </div>
 </template>
+
+<style scoped>
+@media (max-width: 576px) {
+  img.page-header {
+    height: 30vh;
+    object-fit: cover;
+  }
+}
+</style>
