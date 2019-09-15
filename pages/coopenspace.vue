4f4bb5ec80f4fe817f7b1b035b23dcd55a311fda
Alessandro Merletti De Palo <alessandromerlettidepalo@gmail.com>
2019-09-15 18:36:27 +0200

    page reformatting

--- pages/coopenspace.vue
+++ pages/coopenspace.vue
@@ -5,60 +5,85 @@
       src="~/assets/image/page/banners/coopenspace.png"
     />
 
-    <p class="subheading freedom center">COOPERATION AND OPEN SPACE TECHNOLOGY</p>
-    <p class="base">The Open Space Cooperation is used by Cooperacy for enabling system thinking and complex approach in meetings. It merges <a
-        class="af"
-        href="/cooperation"
-      >Cooperation</a> <a
-        class="af"
-        href="/science"
-      >Science</a>, Open Space Technology and the Journey Narratives. It can be organised with a dozens of participants till thousands of people, and it is based on three phases.</p><br><br>
+    <p class="subheading freedom center">
+      COOPERATION AND OPEN SPACE TECHNOLOGY
+    </p>
+    <p class="base">
+      The Open Space Cooperation is used by Cooperacy for enabling system
+      thinking and complex approach in meetings. It merges
+      <a class="af" href="/cooperation">Cooperation</a>
+      <a class="af" href="/science">Science</a>, Open Space Technology and the
+      Journey Narratives. It can be organised with a dozens of participants till
+      thousands of people, and it is based on three phases.
+    </p>
+    <br /><br />
 
     <img
       src="https://cooperacy.org/images/news/5a8a063bccce6.jpg"
       alt="null"
       class="img-responsive"
-    ><br><br>
+    /><br /><br />
 
     <p class="subheading freedom center">I - THE GATHERING</p>
-    <p class="base">In a participants circle, a facilitator asks people to listen to their feelings and to express any concerns to the <a
-        class="ac"
-        href="/cooperation#care"
-      >context</a> surrounding them. The facilitator also explains the Journey Narrative to the circle: the present situation is shown with all the <a
-        class="af"
-        href="/cooperation#freedom"
-      >limitations</a>, the efforts done so far and the positive outcomes. The meeting continues with describing what would have happened in the future if the <a
-        class="ad"
-        href="/cooperation#diversity"
-      >preferences</a> and desires of the participants would come true. Every desire is turned into a proposal. This leads to the ideas ecosystem.</p><br>
+    <p class="base">
+      In a participants circle, a facilitator asks people to listen to their
+      feelings and to express any concerns to the
+      <a class="ac" href="/cooperation#care">context</a> surrounding them. The
+      facilitator also explains the Journey Narrative to the circle: the present
+      situation is shown with all the
+      <a class="af" href="/cooperation#freedom">limitations</a>, the efforts
+      done so far and the positive outcomes. The meeting continues with
+      describing what would have happened in the future if the
+      <a class="ad" href="/cooperation#diversity">preferences</a> and desires of
+      the participants would come true. Every desire is turned into a proposal.
+      This leads to the ideas ecosystem.
+    </p>
+    <br />
 
     <p class="subheading freedom center">II - THE ECOSYSTEM</p>
-    <p class="base">The people start going to the center of the circle in order to express their ideas or to share <a
-        class="ai"
-        href="/cooperation#transparency"
-      >informations</a> by topics. In case of a two-days event, an informative day comes before a final ideas day. The ideas or the informative topics are written on a common board with a time and a space in which they will be discussed. The group is "dissected" and "dies" according to a specific narrative, story or myth. Its parts form new groups and circles. Now and then an "exploration reshuffle" is performed. Coordination and diffusion of knowledge are the transformation tools to solve the exposed problems and the current limitations.</p><br>
+    <p class="base">
+      The people start going to the center of the circle in order to express
+      their ideas or to share
+      <a class="ai" href="/cooperation#transparency">informations</a> by topics.
+      In case of a two-days event, an informative day comes before a final ideas
+      day. The ideas or the informative topics are written on a common board
+      with a time and a space in which they will be discussed. The group is
+      "dissected" and "dies" according to a specific narrative, story or myth.
+      Its parts form new groups and circles. Now and then an "exploration
+      reshuffle" is performed. Coordination and diffusion of knowledge are the
+      transformation tools to solve the exposed problems and the current
+      limitations.
+    </p>
+    <br />
 
-    <div class="center"><iframe
+    <div class="center">
+      <iframe
         width="560"
         height="315"
         src="https://www.youtube.com/embed/9YSy7bUw5EM"
         frameborder="0"
         allow="autoplay; encrypted-media"
         allowfullscreen
-      ></iframe><br><br></div>
+      ></iframe
+      ><br /><br />
+    </div>
 
     <p class="subheading freedom center">III - THE FINAL CIRCLE</p>
-    <p class="base">New <a
-        class="au"
-        href="/cooperation#understanding"
-      >awareness</a> is spread through the participants. The ideas proposed are listed and voted according to: costs and benefits; personal trust; emotional power; transparency; participants commitment; understandability; innovation and creative power of the idea, following the seven conditions of <a
-        class="ae"
-        href="/cooperation"
-      >cooperation</a>. From the voting review a future agenda is made based on a common dictionary, a redistribution of workload and resources and a common roadmap.<br><br>
-      When the organisation need is to use a more quantitative or reflexive approach on a larger scale, without the need of a live event, observing both the people in different locations and their data, the <a
-        class="af"
-        href="/overview"
-      >Overview</a> tool is a better choice.</p><br>
-    <br>
+    <p class="base">
+      New <a class="au" href="/cooperation#understanding">awareness</a> is
+      spread through the participants. The ideas proposed are listed and voted
+      according to: costs and benefits; personal trust; emotional power;
+      transparency; participants commitment; understandability; innovation and
+      creative power of the idea, following the seven conditions of
+      <a class="ae" href="/cooperation">cooperation</a>. From the voting review
+      a future agenda is made based on a common dictionary, a redistribution of
+      workload and resources and a common roadmap.<br /><br />
+      When the organisation need is to use a more quantitative or reflexive
+      approach on a larger scale, without the need of a live event, observing
+      both the people in different locations and their data, the
+      <a class="af" href="/overview">Overview</a> tool is a better choice.
+    </p>
+    <br />
+    <br />
   </div>
 </template>
--- pages/corporate.vue
+++ pages/corporate.vue
@@ -4,54 +4,57 @@
       class="page-header"
       src="~/assets/image/page/banners/corporate.png"
     />
-    <p class="subheading freedom center">COOPERATION IN A COMPETITIVE ENVIRONMENT</p>
-    <p class="base">Thinking in terms of <a
-        class="ae"
-        href="/cooperation"
-      >cooperation</a> and ecosystems enhances the <a
-        class="au"
-        href="/cooperation#understanding"
-      >understanding</a> of complex organizations in order to respect their balance and foster their growth. Increased resilience, organisational capability and innovation can be achieved through ecosystemic thinking and internal cooperation.</p><br>
+    <p class="subheading freedom center">
+      COOPERATION IN A COMPETITIVE ENVIRONMENT
+    </p>
+    <p class="base">
+      Thinking in terms of <a class="ae" href="/cooperation">cooperation</a> and
+      ecosystems enhances the
+      <a class="au" href="/cooperation#understanding">understanding</a> of
+      complex organizations in order to respect their balance and foster their
+      growth. Increased resilience, organisational capability and innovation can
+      be achieved through ecosystemic thinking and internal cooperation.
+    </p>
+    <br />
 
     <p class="subheading freedom center">A PROFITABLE PROFIT SEEKING</p>
-    <p class="base">Profit seeking based only on performance will perform less and bring smaller profits. Cooperacy proposes an ecosystemic model for the management and the facilitation of meetings together with evaluations and interventions related to internal cooperation. The Cooperacy seven dimensions framework measures the <a
-        class="ae"
-        href="/cooperation#equivalence"
-      >Equivalence</a>, <a
-        class="at"
-        href="/cooperation#trust"
-      >Trust</a>, <a
-        class="ac"
-        href="/cooperation#care"
-      >Care</a>, <a
-        class="ai"
-        href="/cooperation#transparency"
-      >Transparency</a>,
-      <a
-        class="af"
-        href="/cooperation#freedom"
-      >Freedom</a>, <a
-        class="au"
-        href="/cooperation#understanding"
-      >Understanding</a> and <a
-        class="ad"
-        href="/cooperation#diversity"
-      >Diversity</a> levels for a in-depth interaction overview. Striking a balance between the different conditions enables new development and productivity in governments, companies and nonprofit organizations.</p><br>
+    <p class="base">
+      Profit seeking based only on performance will perform less and bring
+      smaller profits. Cooperacy proposes an ecosystemic model for the
+      management and the facilitation of meetings together with evaluations and
+      interventions related to internal cooperation. The Cooperacy seven
+      dimensions framework measures the
+      <a class="ae" href="/cooperation#equivalence">Equivalence</a>,
+      <a class="at" href="/cooperation#trust">Trust</a>,
+      <a class="ac" href="/cooperation#care">Care</a>,
+      <a class="ai" href="/cooperation#transparency">Transparency</a>,
+      <a class="af" href="/cooperation#freedom">Freedom</a>,
+      <a class="au" href="/cooperation#understanding">Understanding</a> and
+      <a class="ad" href="/cooperation#diversity">Diversity</a> levels for a
+      in-depth interaction overview. Striking a balance between the different
+      conditions enables new development and productivity in governments,
+      companies and nonprofit organizations.
+    </p>
+    <br />
 
-    <p class="subheading freedom center">COOPERATION SCIENCE NARRATIVE AND APPLICATIONS</p>
-    <p class="base">Cooperacy offers a set of narratives and applications able to foster internal cooperation and ecosystemic balance. <a
-        class="af"
-        href="/coopenspace"
-      >Open Space Cooperation</a> is a narrative-based, sometimes gamified, social ecosystem that brings internal and external forces to collective solutions. <a
-        class="af"
-        href="/overview"
-      >Overview</a> is a quantitative tool useful for focused ecosystemic interventions. <a
-        class="af"
-        href="/cohackathon"
-      >Cohackathon</a> is the more productive way to organise a hackaton for your goals. Cooperation and ecosystemic thinking <a
-        class="af"
-        href="/workshops"
-      >workshops</a> may give the basic and advanced tools to think differently, in a more complex and interconnected way.</p><br>
-    <br>
+    <p class="subheading freedom center">
+      COOPERATION SCIENCE NARRATIVE AND APPLICATIONS
+    </p>
+    <p class="base">
+      Cooperacy offers a set of narratives and applications able to foster
+      internal cooperation and ecosystemic balance.
+      <a class="af" href="/coopenspace">Open Space Cooperation</a> is a
+      narrative-based, sometimes gamified, social ecosystem that brings internal
+      and external forces to collective solutions.
+      <a class="af" href="/overview">Overview</a> is a quantitative tool useful
+      for focused ecosystemic interventions.
+      <a class="af" href="/cohackathon">Cohackathon</a> is the more productive
+      way to organise a hackaton for your goals. Cooperation and ecosystemic
+      thinking <a class="af" href="/workshops">workshops</a> may give the basic
+      and advanced tools to think differently, in a more complex and
+      interconnected way.
+    </p>
+    <br />
+    <br />
   </div>
 </template>
