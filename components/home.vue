f59b60f59f55e93e049e9d4e88fe06b8acb376cc
Ali Akhtari <aaliiaakhtarii@gmail.com>
2019-09-24 18:18:35 +0330

    moved votes modal to projectlist

--- components/home.vue
+++ components/home.vue
@@ -148,51 +148,6 @@
         <br />
       </div>
     </div>
-
-    <!-- votes modal -->
-    <b-modal id="votes-modal" hide-footer hide-header>
-      <p>
-        <img src="../assets/icons/equivalence.svg" />
-        <b>Equivalence:</b>
-        It determines the proportional allocation of funds from the money pool.
-      </p>
-      <p>
-        <img src="../assets/icons/trust.svg" />
-        <b>Trust:</b>
-        Trust votes make the project skip the business plan budget step.
-      </p>
-      <p>
-        <img src="../assets/icons/care.svg" />
-        <b>Care:</b>
-        It’s a like. It gives visibility.
-      </p>
-      <p>
-        <img src="../assets/icons/transparency.svg" />
-        <b>Transparency:</b>
-        This vote is a sort of downvote in pairing projects, or is used to
-        declare hidden information, corruption or spamming in active ones.
-      </p>
-      <p>
-        <img src="../assets/icons/freedom.svg" />
-        <b>Freedom:</b>
-        Freedom-voting means you would like to work for the idea or participate
-        into the decision making. Every project can instead be discussed and
-        observed in full transparency anyways.
-      </p>
-      <p>
-        <img src="../assets/icons/understanding.svg" />
-        <b>Understanding:</b>
-        This vote refers to the idea comprehension/smartness/nerditude and also
-        an information for the project proposer. Understanding gives more
-        visibility and skips the fine tuning phase.
-      </p>
-      <p class="mb-0">
-        <img src="../assets/icons/diversity.svg" />
-        <b>Diversity:</b>
-        When a project has a strong “bizardry” or diversity-votes, it has more
-        opportunities to go randomly in the approving state.
-      </p>
-    </b-modal>
   </div>
 </template>
 
