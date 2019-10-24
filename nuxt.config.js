859f9dd25f9f285ec7ba30808503bed5995c3f8c
Ali Akhtari <aaliiaakhtarii@gmail.com>
2019-10-24 10:14:03 +0330

    added min/max scale to meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,8 @@ module.exports = {
       { charset: 'utf-8' },
       {
         name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
+        content:
+          'width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1'
       },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
