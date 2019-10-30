ef525f593ed201c3a512d3235dfebb08971e8dec
Ali Akhtari <aaliiaakhtarii@gmail.com>
2019-10-30 20:14:29 +0330

    updated news.vue styles to centralize text and images

--- pages/news.vue
+++ pages/news.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="page-container mb-3">
     <br /><img class="page-header" src="~/assets/image/page/banners/news.png" />
-    <div class="row mt-2" v-for="news in this.$store.state.news" :key="news.id">
+    <div
+      class="row mt-2 page-container"
+      v-for="news in this.$store.state.news"
+      :key="news.id"
+    >
       <p class="subheading freedom center space up margin">{{ news.title }}</p>
       <br />
-      <p><img class="news" :src="newsImage(news)" v-if="news.img" /></p>
+      <img class="news" :src="newsImage(news)" v-if="news.img" />
       <br />
       <span class="base" v-html="news.content"></span>
     </div>
